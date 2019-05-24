f6753c36440c5c94e38b91b8e866316e40a59535
alleriaGuo <532492513@qq.com>
2019-05-24 14:15:01 +0800

    'mt-init'

---
+++ src/components/header/topBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-row class="top">
+    <el-col :span="10">
+      <geo />
+    </el-col>
+    <el-col :span="14">
+      <m-nav />
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+  import Geo from "@/components/header/geo.vue";
+  import MNav from "@/components/header/nav.vue";
+  export default {
+    name: 'topBar',
+    components:{
+      Geo,
+      MNav
+    }
+  }
+</script>
+
+<style lang="scss">
+@import "@/assets/css/public/header/topbar.scss";
+</style>
