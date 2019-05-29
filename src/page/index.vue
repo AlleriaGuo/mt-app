c055deaa7192d64345aeb2d9269147163a8bcfb8
alleriaGuo <532492513@qq.com>
2019-05-29 09:59:01 +0800

    首页

--- src/page/index.vue
+++ src/page/index.vue
@@ -1,13 +1,32 @@
 <template>
-    <div>index</div>
+    <div class="page-index">
+      <el-row>
+        <el-col :span="5">
+          <m-menu />
+        </el-col>
+        <el-col :span="19">
+          <m-life />
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="24">
+          container
+        </el-col>
+      </el-row>
+    </div>
 </template>
 
 <script>
+import MMenu from '@/components/index/menu.vue'
+import MLife from '@/components/index/life.vue'
 export default {
-  name: 'index'
+  components: {
+    MMenu,
+    MLife
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+  @import "@/assets/css/index/index.scss";
 </style>
